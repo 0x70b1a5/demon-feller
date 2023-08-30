495fee76984f2c0407c64de20fffaa55a760b9fa
Tobias Merkle <tobias@branchmessenger.com>
2023-08-30 11:29:52 -0400

    fix enemy spawns

--- src/game/constants/enemies.ts
+++ src/game/constants/enemies.ts
@@ -6,9 +6,9 @@ const enemyWeights = [
   { type: EnemyType.Glutton, weight: 1 },
   { type: EnemyType.Imp, weight: 1 },
   { type: EnemyType.Soul, weight: 1 },
-  { type: EnemyType.Hothead, weight: 1000 },
+  { type: EnemyType.Hothead, weight: 1 },
   { type: EnemyType.ImpMother, weight: 0.5 },
-  { type: EnemyType.Covetor, weight: 1 },
+  { type: EnemyType.Covetor, weight: 0.5 },
 ]
 
 export default enemyWeights
